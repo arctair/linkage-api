e30fae9d1414ec1052ce216737438dca59b637d1
tyler <tyler@cruftbusters.com>
2022-03-08 14:35:02 -0600

    zone client type hints

--- test/zone_client.py
+++ test/zone_client.py
@@ -2,22 +2,22 @@ class ZoneClient:
     def __init__(self, client):
         self.client = client
 
-    def get(self, id, expect_status=200):
+    def get(self, id: str, expect_status: int = 200) -> dict:
         response = self.client.get(f'/v1/zones/{id}')
         assert response.status_code == expect_status
         return response.json
 
-    def create(self, id, expect_status=200):
+    def create(self, id: str, expect_status: int = 200) -> dict:
         response = self.client.post(f'/v1/zones/{id}')
         assert response.status_code == expect_status
         return response.json
 
-    def delete(self, id, expect_status=200):
+    def delete(self, id: str, expect_status: int = 200) -> dict:
         response = self.client.delete(f'/v1/zones/{id}')
         assert response.status_code == expect_status
         return response.json
 
-    def update(self, id, zone, expect_status=200):
+    def update(self, id: str, zone: dict, expect_status: int = 200) -> dict:
         response = self.client.put(f'/v1/zones/{id}', json=zone)
         assert response.status_code == expect_status
         return response.json
